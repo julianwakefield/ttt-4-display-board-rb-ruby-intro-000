152c3b3efc863ad33d729129bf86ed02b8e42bd8
Julian Wakefield <julianwakefield@outlook.com>
2019-08-27 19:30:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,5 @@
-require "pry"
 def display_board(board)
 
-#puts "   |   |   ", "-----------", "   |   |   " , "-----------", "   |   |   "
-binding.pry
+puts "#{board[0]}   |#{board[1]}   |#{board[2]}   ", "-----------", "#{board[3]}   |#{board[4]}   |#{board[5]}   " , "-----------", "#{board[6]}   |#{board[7]}   |#{board[8]}   "
 
 end
