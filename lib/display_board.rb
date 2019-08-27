e8e01e4d66e9f3ccadacee1ddeadd2ed627e8e43
Julian Wakefield <julianwakefield@outlook.com>
2019-08-27 19:29:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+require "pry"
+def display_board(board)
+
+#puts "   |   |   ", "-----------", "   |   |   " , "-----------", "   |   |   "
+binding.pry
+
+end
